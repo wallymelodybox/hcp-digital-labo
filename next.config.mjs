58bc88a4e250651f3cb48ab42fb74b476777ace3
wallymelodybox <42175136+wallymelodybox@users.noreply.github.com>
2026-02-28 15:19:03 +0100

    carte en story

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const __dirname = path.dirname(__filename);
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   turbopack: {
-    root: 'C:\\Users\\wally\\OneDrive\\Documents\\GitHub',
+    root: __dirname,
   },
   images: {
     remotePatterns: [
